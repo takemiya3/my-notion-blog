f631222ae3889702dd1665f1c290d0c3249d4e5a
takemiya3 <taketai0709@gmail.com>
2026-01-30 04:46:41 +0900

    fix: AffiliateWidget dangerouslySetInnerHTMLの構文エラーを修正

--- components/AffiliateWidget.tsx
+++ components/AffiliateWidget.tsx
@@ -1,42 +1,35 @@
 'use client'
 
-import Script from 'next/script';
-
-type AffiliateWidgetProps = {
-  dataId?: string;
-  html?: string;
-  type?: 'DMM' | 'その他';
-  className?: string;
-}
-
 export default function AffiliateWidget({ 
   dataId,
   html,
-  type = 'DMM',
   className = "mt-12 mb-8" 
-}: AffiliateWidgetProps) {
-  // HTMLが直接渡された場合はそれをレンダリング
+}: {
+  dataId?: string;
+  html?: string;
+  className?: string;
+}) {
+  // HTMLが渡された場合
   if (html) {
     return (
       <div 
         className={className}
-        dangerouslySetInnerHTML={{__html: html}} 
+        dangerouslySetInnerHTML=__html: html
       />
     );
   }
 
-  // dataIdが渡された場合はDMMウィジェットをレンダリング
-  if (dataId && type === 'DMM') {
+  // dataIdが渡された場合
+  if (dataId) {
     return (
       <div className={className}>
-        <ins 
-          className="dmm-widget-placement" 
-          data-id={dataId} 
-          style={{background: 'transparent'}}
-        />
-        <Script
-          src="https://widget-view.dmm.co.jp/js/placement.js"
-          strategy="afterInteractive"
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <ins class="dmm-widget-placement" data-id="${dataId}" style="background:transparent"></ins>
+              <script src="https://widget-view.dmm.co.jp/js/placement.js" class="dmm-widget-scripts" data-id="${dataId}"></script>
+            `
+          }}
         />
       </div>
     );
